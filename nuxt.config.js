42b20f691edd9b29009011b6e56d83a1a2a39048
Lukas Schönbeck <lukas.schoenbeck@gmx.de>
2021-07-24 16:31:15 +0200

    remove bootstrap add buefy

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,7 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/bootstrap
-    'bootstrap-vue/nuxt',
+    'nuxt-buefy',
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
